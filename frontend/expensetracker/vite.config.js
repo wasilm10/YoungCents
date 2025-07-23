b963c69b71036deccb7015f53a1427b76fe3b886
Wasi Ahmad <wasi.cd22@bmsce.ac.in>
2025-07-24 03:25:50 +0530

    Update vite.config.js

--- frontend/expensetracker/vite.config.js
+++ frontend/expensetracker/vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './', // ✅ ensures relative paths for static assets
+  build: {
+    outDir: 'dist', // ✅ tells Vite where to build the app
+  },
 })
